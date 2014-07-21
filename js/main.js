c594f0b95e51bfc4ee3fc2b58438397b82dfce41
heldrida <heldrida@gmail.com>
2014-07-21 17:29:08 +0000

    created a model, with a few default properties, and methods

--- js/main.js
+++ js/main.js
@@ -1,9 +1,15 @@
+// View
+
 SearchView = Backbone.View.extend({
 	initialize: function(){
 		this.render();
 	},
 	render: function(){
-		var template = _.template( $("#search_template").html(), {});
+		var variables = {
+			search_label: "My Search"
+		};
+		var template = _.template( $("#search_template").html(), variables);
+
 		this.$el.html( template );
 	},
 	events: {
@@ -17,3 +23,38 @@ SearchView = Backbone.View.extend({
 var search_view = new SearchView({
 	el: $("#search_container")
 });
+
+// Model
+
+Person = Backbone.Model.extend({
+	defaults: {
+		name: 'Feturs',
+		age: 0,
+		child: ''
+	},
+	initialize: function(){
+		console.log("Hello model");
+		this.on("change:name", function(model){
+			var name = model.get("name");
+			console.log("Changed my name to " + name);
+		});
+	},
+	adopt: function( newChildsName ){
+		this.set({
+			child: newChildsName
+		});
+	}
+});
+
+var person = new Person({
+	name: "Thomans",
+	age: 67
+});
+
+setTimeout(function(){
+
+	person.set({
+		name: 'Stewie Griffin'
+	});
+
+}, 2000);
