f72847b3c709b92db74dfb0a786425f28546969a
heldrida <heldrida@gmail.com>
2014-07-21 23:36:32 +0000

    created validation

--- js/main.js
+++ js/main.js
@@ -33,28 +33,103 @@ Person = Backbone.Model.extend({
 		child: ''
 	},
 	initialize: function(){
+		
 		console.log("Hello model");
-		this.on("change:name", function(model){
-			var name = model.get("name");
-			console.log("Changed my name to " + name);
+		
+		this.on("invalid", function( model, error ){
+			console.log("error found:");
+			console.log( error );
 		});
+
 	},
 	adopt: function( newChildsName ){
 		this.set({
 			child: newChildsName
 		});
+	},
+	validate: function( attributes ){
+		if ( attributes.age < 0 && attributes.name != "Dr Manhatten" ){
+			return "You can't be negative years old";
+		}
 	}
 });
 
-var person = new Person({
-	name: "Thomans",
-	age: 67
+var person = new Person();
+
+person.set({
+	name: "Mary Poppins",
+	age: -1
+}, {
+	validate: true
 });
 
-setTimeout(function(){
 
-	person.set({
-		name: 'Stewie Griffin'
-	});
+/*
+// Interacting wih the server
 
-}, 2000);
+var UserModel = Backbone.Model.extend({
+	urlRoot: 'http://tests.punkbit.com:2403/users',
+	defaults: {
+		username: '',
+		password: '',
+		email: ''
+	}
+});
+
+// Post example
+
+var user = new UserModel();
+
+var userDetails = {
+	username: 'Thomas',
+	email: 'thomasalwas@gmail.com',
+	password: 'testpassword'
+};
+
+// Get example
+
+user.save( userDetails, {
+	success: function( user ){
+		console.log("save return data:");
+		console.log(user.toJSON());
+	}
+});
+
+var user = new UserModel({ id: 'b910c34776076895' });
+
+user.fetch({
+	success: function( user ){
+		console.log( user.toJSON() );
+	}
+});
+
+// PUT (Update) example
+
+var user = new UserModel({
+	id: 'b910c34776076895',
+	username: 'Peter Griffin',
+	email: 'peter.griffins@supersitta.com'
+});
+
+user.save({
+		username: 'Petter Isgriffin'
+	}, {
+		success: function( model ){
+			console.log( user.toJSON() );
+		}
+	}
+);
+
+// DELETE example
+
+var user = new UserModel({
+	id: 'b910c34776076895'
+});
+
+user.destroy({
+	success: function(){
+		console.log('Destroyed!');
+	}
+});
+*/
+
