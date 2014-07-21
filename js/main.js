e3cd8e335e95b1e453f419779f8cf76fe5494d56
heldrida <heldrida@gmail.com>
2014-07-21 15:44:03 +0000

    created basic html structure, main js file with view and some event listeners, plus one template container and template

---
+++ js/main.js
@@ -0,0 +1,19 @@
+SearchView = Backbone.View.extend({
+	initialize: function(){
+		this.render();
+	},
+	render: function(){
+		var template = _.template( $("#search_template").html(), {});
+		this.$el.html( template );
+	},
+	events: {
+		"click input[type=button]": "doSearch"
+	},
+	doSearch: function( event ){
+		console.log( "Search for " + $("#search_input").val() );
+	}
+});
+
+var search_view = new SearchView({
+	el: $("#search_container")
+});
